0a5d462fcf89f39b7816f39dcc92abcf239e769c
monikads252001 <monikads2460@gmail.com>
2023-07-17 23:15:09 +0530

    files added

---
+++ library/admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2022 Online Library Management System |<a href="https://library.com/" target="_blank"  > Designed by : MONIKA.B & VISMAYA.N.U</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
